778de39c3eb8311a2c5274b4f26ff12ec6870a70
Abdullah Bawazeer <abdullahbawazeer611@gmail.com>
2026-07-07 11:35:38 +0300

    add rating to book details

--- lib/Features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/Features/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:book_app/Features/home/presentation/views/widgets/custom_book_details_app_bar.dart';
 import 'package:book_app/Features/home/presentation/views/widgets/custom_book_item.dart';
+import 'package:book_app/Features/home/presentation/views/widgets/rating_book.dart';
 import 'package:book_app/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 
@@ -34,6 +35,8 @@ class BookDetailsViewBody extends StatelessWidget {
               ),
             ),
           ),
+          const SizedBox(height: 18),
+          const BookRating(mainAxisAlignment: MainAxisAlignment.center),
         ],
       ),
     );
