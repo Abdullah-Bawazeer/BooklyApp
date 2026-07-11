20773edebc5d70fa388251b96fa5f5244b98dc7c
Abdullah Bawazeer <abdullahbawazeer611@gmail.com>
2026-07-11 10:57:47 +0300

    search view done

--- lib/Features/search/presentation/views/widgets/search_view_body.dart
+++ lib/Features/search/presentation/views/widgets/search_view_body.dart
@@ -1,4 +1,6 @@
+import 'package:book_app/Features/home/presentation/views/widgets/best_seller_list_view_item.dart';
 import 'package:book_app/Features/search/presentation/views/widgets/custom_search_text_field.dart';
+import 'package:book_app/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 
 class SearchViewBody extends StatelessWidget {
@@ -8,7 +10,32 @@ class SearchViewBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 20),
-      child: Column(children: [const CustomSearchTextField()]),
+      child: Column(
+        children: const [
+          CustomSearchTextField(),
+          SizedBox(height: 16),
+          Text('Search Result', style: Styles.textStyle18),
+          Expanded(child: SearchResultListView()),
+        ],
+      ),
+    );
+  }
+}
+
+class SearchResultListView extends StatelessWidget {
+  const SearchResultListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      padding: EdgeInsets.zero,
+      itemCount: 10,
+      itemBuilder: (context, index) {
+        return const Padding(
+          padding: EdgeInsets.symmetric(vertical: 10),
+          child: BookListViewItem(),
+        );
+      },
     );
   }
 }
