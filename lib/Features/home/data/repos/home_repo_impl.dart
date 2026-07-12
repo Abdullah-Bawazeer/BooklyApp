f2d5beda0265426a8ef128bfde2a32337578f27c
Abdullah Bawazeer <abdullahbawazeer611@gmail.com>
2026-07-12 11:11:25 +0300

    create api service

---
+++ lib/Features/home/data/repos/home_repo_impl.dart
@@ -0,0 +1,16 @@
+import 'package:book_app/Features/home/data/models/book_model/book_model.dart';
+import 'package:book_app/Features/home/data/repos/home_repo.dart';
+import 'package:book_app/core/errors/failures.dart';
+import 'package:dartz/dartz.dart';
+
+class HomeRepoImpl extends HomeRepo {
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks() {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() {
+    throw UnimplementedError();
+  }
+}
