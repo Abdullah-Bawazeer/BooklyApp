cea0250cb6c7c6a4e386872ebd417996963db3f6
Abdullah Bawazeer <abdullahbawazeer611@gmail.com>
2026-07-18 10:33:30 +0300

    fetch similar books done

--- lib/Features/home/presentation/views/widgets/similar_box_list_view.dart
+++ lib/Features/home/presentation/views/widgets/similar_box_list_view.dart
@@ -1,24 +1,38 @@
+import 'package:book_app/Features/home/presentation/manager/similar_books_cubit/similar_books_cubit.dart';
 import 'package:book_app/Features/home/presentation/views/widgets/custom_book_item.dart';
 import 'package:book_app/core/utils/assets.dart';
+import 'package:book_app/core/widgets/custom_error_widget.dart';
+import 'package:book_app/core/widgets/custom_loading_indicator.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SimilerBooksListView extends StatelessWidget {
   const SimilerBooksListView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      //height: MediaQuery.of(context).size.height * 0.2 * 1,
-      height: MediaQuery.of(context).size.height * .15,
-      child: ListView.builder(
-        scrollDirection: Axis.horizontal,
-        itemBuilder: ((context, index) {
-          return const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 5),
-            child: CustomBookImage(imageUrl: AssetsData.testImage),
+    return BlocBuilder<SimilarBooksCubit, SimilarBooksState>(
+      builder: (context, state) {
+        if (state is SimilarBooksSuccess) {
+          return SizedBox(
+            //height: MediaQuery.of(context).size.height * 0.2 * 1,
+            height: MediaQuery.of(context).size.height * .15,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemBuilder: ((context, index) {
+                return const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 5),
+                  child: CustomBookImage(imageUrl: AssetsData.testImage),
+                );
+              }),
+            ),
           );
-        }),
-      ),
+        } else if (state is SimilarBooksFailure) {
+          return CustomErrorWidget(errMessage: state.errMessage);
+        } else {
+          return const CustomLoadingIndicator();
+        }
+      },
     );
   }
 }
--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,7 +1,12 @@
 import 'package:book_app/Features/Splash/Presentation/views/splash_view.dart';
+import 'package:book_app/Features/home/data/models/book_model/book_model.dart';
+import 'package:book_app/Features/home/data/repos/home_repo_impl.dart';
+import 'package:book_app/Features/home/presentation/manager/similar_books_cubit/similar_books_cubit.dart';
 import 'package:book_app/Features/home/presentation/views/book_details_view.dart';
 import 'package:book_app/Features/home/presentation/views/home_view.dart';
 import 'package:book_app/Features/search/presentation/views/search_view.dart';
+import 'package:book_app/core/utils/serivce_locator.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 
 abstract class AppRouter {
@@ -14,7 +19,10 @@ abstract class AppRouter {
       GoRoute(path: kHomeView, builder: (context, state) => const HomeView()),
       GoRoute(
         path: kBookDetailsView,
-        builder: (context, state) => const BookDetailsView(),
+        builder: (context, state) => BlocProvider(
+          create: (context) => SimilarBooksCubit(getIt.get<HomeRepoImpl>()),
+          child: BookDetailsView(bookModel: state.extra as BookModel),
+        ),
       ),
       GoRoute(
         path: kSearchView,
